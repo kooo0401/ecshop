4c36fc854aad5bee0868a14c43168bb0eff0c546
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2020-01-10 19:28:07 +0900

    template_edit

--- cart/views.py
+++ cart/views.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 # stripe(支払い機能)
 import stripe
 from django.conf import settings
+from order.models import Order, OrderItem
 
 
 def _cart_id(request):
@@ -54,16 +55,63 @@ def cart_detail(request, total=0, counter=0, cart_items = None):
         try:
             token = request.POST['stripeToken']
             email = request.POST['stripeEmail']
+            billingName = request.POST['stripeBillingName']
+            billingAddress1 = request.POST['stripeBillingAddressLine1']
+            billingcity = request.POST['stripeBillingAddressCity']
+            billingPostcode = request.POST['stripeBillingAddressZip']
+            billingCountry = request.POST['stripeBillingAddressCountryCode']
+            shippingName = request.POST['stripeShippingName']
+            shippingAddress1 = request.POST['stripeShippingAddressLine1']
+            shippingcity = request.POST['stripeShippingAddressCity']
+            shippingPostcode = request.POST['stripeShippingAddressZip']
+            shippingCountry = request.POST['stripeShippingAddressCountryCode']
             customer = stripe.Customer.create(
                         email=email,
                         source=token
              )
             charge = stripe.Charge.create(
                        amount=stripe_total,
-                       currency='jpy',
+                       currency='GBP',
                        description=description,
                        customer=customer.id
               )
+            '''Creating the order'''
+            try:
+                order_details = Order.objects.create(
+                        token = token,
+                        total = total,
+                        emailAddress = email,
+                        billingName = billingName,
+                        billingAddress1 = billingAddress1,
+                        billingCity = billingcity,
+                        billingPostcode = billingPostcode,
+                        billingCountry = billingCountry,
+                        shippingName = shippingName,
+                        shippingAddress1 = shippingAddress1,
+                        shippingCity = shippingcity,
+                        shippingPostcode = shippingPostcode,
+                        shippingCountry = shippingCountry
+                    )
+                order_details.save()
+                for order_item in cart_items:
+                    oi = OrderItem.objects.create(
+                            product = order_item.product.name,
+                            quantity = order_item.quantity,
+                            price = order_item.product.price,
+                            order = order_details
+                        )
+                    oi.save()
+                    '''Reduce stock when order is placed or saved'''
+                    products = Product.objects.get(id=order_item.product.id)
+                    products.stock = int(order_item.product.stock - order_item.quantity)
+                    products.save()
+                    order_item.delete()
+                    '''The terminal will print this message when the order is saved'''
+                    print('The order has been created')
+                return redirect('shop:allProdCat')
+            except ObjectDoesNotExist:
+                pass
+
         except stripe.error.CardError as e:
             return False,e
 
