16f0beab113902438a9fe26fde8ede5214f9b1ff
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2020-01-11 00:25:31 +0900

    order_history_creating

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Category, Product
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
