0dcba6753604e036e8bb69b0784af2378e485190
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2020-01-10 10:50:39 +0900

    toppage_create_done

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -13,6 +14,8 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
 
 
+    def get_url(self):
+        return reverse('shop:products_by_category', args=[self.slug])
     def __str__(self):
         return '{}'.format(self.name)
 
